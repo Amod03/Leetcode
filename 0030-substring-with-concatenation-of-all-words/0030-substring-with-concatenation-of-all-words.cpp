3faa12639de1d861f503009f021c17649c8d6eb6
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-03-28 00:37:14 +0530

    Time: 896 ms (28.00%), Space: 260.3 MB (36.89%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int>ans;
+        if(s.length()==0 || words.size()==0 || words.size()*words[0].length()>s.length())
+            return ans;
+        unordered_map<string,int>wordsfreq;
+        for(int i=0;i<words.size();i++)
+        {
+            wordsfreq[words[i]]++;
+        }
+        int n=words.size();
+        int l=words[0].length();
+        unordered_map<string,int>temp;
+        for(int i=0;i<=s.length()-n*l;i++)
+        {
+            for(int j=i;j<i+n*l;j+=l)
+            {
+                temp[s.substr(j,l)]++;
+            }
+            int flag=1;
+            for(int j=0;j<n;j++)
+            {
+                if(temp.find(words[j])==temp.end() || temp[words[j]]!=wordsfreq[words[j]])
+                {
+                    flag=0;
+                    break;
+                }
+            }
+                if(flag==1)
+                    ans.push_back(i);
+                temp.clear();
+        }
+        return ans;
+    }
+};
