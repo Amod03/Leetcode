bea6a489160ba6dd3ea919671639f19ec801cc5a
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-04-03 01:49:49 +0530

    Time: 42 ms (58.13%), Space: 18.3 MB (99.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n=intervals.size();
-        vector<vector<int>>merge;
-        if(intervals.size()==0)
-            return merge;
+
         sort(intervals.begin(),intervals.end());
         int index=0;
-        for(int i=1;i<intervals.size();i++)
+        for(int i=1;i<n;i++)
         {
             if(intervals[i][0]<= intervals[index][1])
                 intervals[index][1]=max(intervals[i][1],intervals[index][1]);
@@ -16,7 +14,6 @@ public:
                 intervals[index]=intervals[i];
             }
         }
-        cout<<index;
         for(int i=0;i<(n-index-1);i++)
         {
             intervals.pop_back();
