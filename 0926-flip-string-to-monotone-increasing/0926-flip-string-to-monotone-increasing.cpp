7e2e22f31f8a6b11d3c523863a0b63691f477e9c
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-04-06 14:38:54 +0530

    Time: 917 ms (5.11%), Space: 34 MB (9.97%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int n = s.size();
+        vector<int> pre;
+        vector<int> suf;
+        pre.push_back(0);
+        suf.push_back(0);
+        for(int i=1;i<=n;i++)
+        {
+            if(s[i-1]=='0')
+                pre.push_back(pre[i-1]);
+            else{
+                int k=pre[i-1]+1;
+                pre.push_back(k);
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='0')
+                suf.push_back(suf[n-i-1]+1);
+            else
+                suf.push_back(suf[n-i-1]);
+        }
+        reverse(suf.begin(), suf.end());
+        for(int i=0;i<=n;i++)
+        {
+          cout<<suf[i]<<endl;
+        }
+        int ans = INT_MAX;
+        for(int i=1;i<=n;i++)
+        {
+            int p = pre[i-1]+suf[i];
+            ans=min(ans, p);
+        }
+        return ans;
+    }
+    
+};
