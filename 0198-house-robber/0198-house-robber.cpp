49ed3f62838b10ab36995bb8b0f1ac263d9d905f
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-05-23 15:20:16 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (82.23%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& arr) {
+    int n = arr.size();
+    long long int prev = arr[0];
+    long long int prev2 =0;
+    
+    for(int i=1; i<n; i++){
+        long long int pick = arr[i];    
+        pick += prev2;
+        int long long nonPick = 0 + prev;
+        
+        long long int cur_i = max(pick, nonPick);
+        prev2 = prev;
+        prev= cur_i;
+        
+    }
+    return prev;
+    }
+};
