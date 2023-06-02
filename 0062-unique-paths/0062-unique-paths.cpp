87d6f34cb66e037d4ae52c7d38b177b0ae8a69c0
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-06-02 23:24:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (68.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N=n+m-2;
-        int r=m-1;
-        double res=1;
+        vector<int>prev(n,0);
         
-        for(int i=1;i<=r;i++)
+        for(int i=0;i<m;i++)
         {
-            res=res*(N-r+i)/i;
-        }
-        return (int)res;
+            vector<int>cur(n,0);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                    cur[i]=1;
+                else
+                {
+                    int left=0;
+                    if(j>0)
+                        left=cur[j-1];
+                cur[j]=left+prev[j];
+                }
+            }
+            prev=cur;
+         }
+        return prev[n-1];
     }
 };
