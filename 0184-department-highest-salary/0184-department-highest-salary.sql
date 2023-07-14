b2f6f25aabb59c38692f6b2516e7d666c538f55f
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-07-14 10:14:53 +0530

    Time: 1057 ms (99.30%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,8 @@
+SELECT d.name AS Department, e.name AS Employee, e.Salary
+FROM Employee e
+INNER JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
