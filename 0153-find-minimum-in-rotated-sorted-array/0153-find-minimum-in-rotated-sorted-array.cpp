7b2411202057ec5c427860ec049cbe18cd405060
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-08-13 19:20:32 +0530

    Time: 3 ms (73.07%), Space: 10.1 MB (45.97%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    int getpivot(vector<int>&arr,int n)
+    {
+ int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+    }
+
+
+    int findMin(vector<int>& nums) {
+      int k=getpivot(nums,nums.size());
+      return k;
+    }
+};
