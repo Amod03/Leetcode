2ab7077f9117313e3e274d05ea84e08dafcc4500
Amod03 <121477867+Amod03@users.noreply.github.com>
2023-08-22 01:31:23 +0530

    Time: 10 ms (97.23%), Space: 10.6 MB (48.71%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -3,7 +3,7 @@ public:
     long long subArrayRanges(vector<int>& A) {
         long res = 0, n = A.size(), j, k;
         stack<int> s;
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 0; i <= n; i++) {
             while (!s.empty() && A[s.top()] > (i == n ? -2e9 : A[i])) {
                 j = s.top(), s.pop();
                 k = s.empty() ? -1 : s.top();
@@ -12,7 +12,7 @@ public:
             s.push(i);
         }
         s = stack<int>();
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 0; i <= n; i++) {
             while (!s.empty() && A[s.top()] < (i == n ? 2e9 : A[i])) {
                 j = s.top(), s.pop();
                 k = s.empty() ? -1 : s.top();
