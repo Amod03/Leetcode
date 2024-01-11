4f93735f6ae4cb1131917d7e76dfb027a23caa40
Amod03 <121477867+Amod03@users.noreply.github.com>
2024-01-11 18:23:49 +0530

    Time: 16 ms (44.77%), Space: 20 MB (68.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,7 +6,10 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             if(count==0)
+            {
                 ele=nums[i];
+            }
+            
             
                 if(ele==nums[i])
                     count++;
