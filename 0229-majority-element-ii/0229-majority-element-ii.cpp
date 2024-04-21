f9f7ea38b9c03f66e813586e3b6074e6b8f06442
Amod03 <121477867+Amod03@users.noreply.github.com>
2024-04-21 10:51:27 +0530

    Time: 7 ms (83.27%), Space: 18.3 MB (92.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,7 +6,6 @@ public:
     int cnt1 = 0, cnt2 = 0; // counts
     int el1 = INT_MIN; // element 1
     int el2 = INT_MIN; // element 2
-
     // applying the Extended Boyer Moore's Voting Algorithm:
     for (int i = 0; i < n; i++) {
         if (cnt1 == 0 && el2 != v[i]) {
@@ -23,11 +22,7 @@ public:
             cnt1--, cnt2--;
         }
     }
-
-    vector<int> ls; // list of answers
-
-    // Manually check if the stored elements in
-    // el1 and el2 are the majority elements:
+    vector<int> ls; 
     cnt1 = 0, cnt2 = 0;
     for (int i = 0; i < n; i++) {
         if (v[i] == el1) cnt1++;
@@ -37,11 +32,6 @@ public:
     int mini = int(n / 3) + 1;
     if (cnt1 >= mini) ls.push_back(el1);
     if (cnt2 >= mini) ls.push_back(el2);
-
-    // Uncomment the following line
-    // if it is told to sort the answer array:
-    // sort(ls.begin(), ls.end()); //TC --> O(2*log2) ~ O(1);
-
     return ls;
     }
 };
