b90f8d73b92be341fa9edb3fb13b976ff4425802
Amod03 <121477867+Amod03@users.noreply.github.com>
2024-04-25 23:18:53 +0530

    Time: 26 ms (53.71%), Space: 94.4 MB (75.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size()<nums1.size())
-            return findMedianSortedArrays(nums2,nums1);
-        
-        int n1=nums1.size();
-        int n2=nums2.size();
-        int low=0,high=n1;
-        
-        while(low<=high)
-        {
-            int cut1=(low+high)>>1;
-            int cut2=(n1+n2+1)/2-cut1;
-            
-            int left1=cut1==0?INT_MIN:nums1[cut1-1];
-            int left2=cut2==0?INT_MIN:nums2[cut2-1];
-            
-            int right1=cut1==n1?INT_MAX:nums1[cut1];
-            int right2=cut2==n2?INT_MAX:nums2[cut2];
-            
-            if(left1<=right2 && left2<=right1)
-            {
-                if((n1+n2)%2==0)
-                    return (max(left1,left2)+min(right1,right2))/2.0;
-                else
-                    return max(left1,left2);
-            }
-            else if(left1>right2)
-                high=cut1-1;
-            else
-                low=cut1+1;
-            
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+       int n1 = a.size(), n2 = b.size();
+    //if n1 is bigger swap the arrays:
+    if (n1 > n2) return findMedianSortedArrays(b, a);
+
+    int n = n1 + n2; //total length
+    int left = (n1 + n2 + 1) / 2; //length of left half
+    //apply binary search:
+    int low = 0, high = n1;
+    while (low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        //calculate l1, l2, r1 and r2;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if (mid1 < n1) r1 = a[mid1];
+        if (mid2 < n2) r2 = b[mid2];
+        if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if (l1 <= r2 && l2 <= r1) {
+            if (n % 2 == 1) return max(l1, l2);
+            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
         }
-        return 0.0;
+
+        //eliminate the halves:
+        else if (l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0.0; //dummy statement
     }
 };
