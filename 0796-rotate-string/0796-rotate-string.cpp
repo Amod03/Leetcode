fe35cbb25725b043e6f07466843f5fca26d190ab
Amod03 <121477867+Amod03@users.noreply.github.com>
2024-06-24 18:40:12 +0530

    Time: 3 ms (48.24%), Space: 7.4 MB (62.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-    if (s.length() != goal.length()) {
-        return false;
+    bool rotateString(string A, string B) {
+    return A.size() == B.size() && (A + A).find(B) != string::npos;
     }
-
-    string concat = s + s;
-
-    for (int i = 0; i < s.length(); ++i) {
-        int j = 0;
-        while (j < goal.length() && concat[i + j] == goal[j]) {
-            ++j;
-        }
-        if (j == goal.length()) {
-            return true; // Found the goal as a substring of concat
-        }
-    }
-
-    return false; // goal is not a substring of concat
-}
 };
