33bc2f4e269d17b35af29201da0db339e9ca74f6
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-24 18:57:57 +0530

    Time: 12 ms (29.58%), Space: 18.6 MB (42.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -6,7 +6,6 @@ public:
     int cnt1 = 0, cnt2 = 0; // counts
     int el1 = INT_MIN; // element 1
     int el2 = INT_MIN; // element 2
-    // applying the Extended Boyer Moore's Voting Algorithm:
     for (int i = 0; i < n; i++) {
         if (cnt1 == 0 && el2 != v[i]) {
             cnt1 = 1;
