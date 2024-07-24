b061f273d8ef06efa1df408802e6e647afc5ff1e
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-24 12:42:21 +0530

    Time: 5 ms (15.79%), Space: 10 MB (10.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,23 +4,17 @@ public:
         int low=0;
         int mid=0;
         int high=nums.size()-1;
-        while(mid<=high)
-        {
-            if(nums[mid]==0)
-            {
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else if(nums[mid]==2)
-            {
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+        while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[mid],nums[low]);
+            low++;
+            mid++;
+        }else if(nums[mid]==1){
+            mid++;
+        }else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
         }
     }
 };
