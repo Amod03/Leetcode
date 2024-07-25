77fb03c4936a5a7bfe341cba729362569391afcc
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-25 12:04:50 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (15.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,6 +10,21 @@ public:
     }
     int uniquePaths(int n, int m) {
         vector<vector<int>>dp(n,vector<int>(m,-1));
-     return solve(n-1,m-1,dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+             if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+                continue; 
+            }
+            int top=0;
+            if(i>0)
+            top=dp[i-1][j];
+            int left=0;
+            if(j>0)
+            left=dp[i][j-1];
+            dp[i][j]=top+left;
+            }
+        }
+     return dp[n-1][m-1];
     }
 };
