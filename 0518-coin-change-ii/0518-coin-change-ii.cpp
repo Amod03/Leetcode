b228a48ed574cba6ecf183fe5b0785c848c22329
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-26 18:19:45 +0530

    Time: 10 ms (58.53%), Space: 20.2 MB (21.36%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
+    
+    int solve(int n,int target,vector<int>&coins,vector<vector<int>>&dp){
+        if(n==0){
+            if(target % coins[0] == 0)
+            return 1; 
+        else
+            return 0; 
+        }
+        if(dp[n][target]!=-1)return dp[n][target];
+        int take=0;
+        if(coins[n]<=target)
+        take=solve(n,target-coins[n],coins,dp);
+        int notTake=solve(n-1,target,coins,dp);
+        return dp[n][target]=take+notTake;
+    }
+    
     int change(int target, vector<int>& coins) {
         int n=coins.size();
-        vector<int>prev(target+1,0),cur(target+1,0);
-        
-        for(int t=0;t<=target;t++)
-        {
-            if(t%coins[0]==0)
-                prev[t]=(t%coins[0]==0);
+        vector<vector<int>>dp(coins.size(),vector<int>(target+1,-1));
+        return solve(n-1,target,coins,dp);
         }
-        for(int ind=1;ind<n;ind++)
-        {
-            for(int t=0;t<=target;t++)
-            {
-                int nottake=prev[t];
-                int take=0;
-                if(coins[ind]<=t)
-                {
-                    take=cur[t-coins[ind]];
-                }
-                cur[t]=take+nottake;
-            }
-            prev=cur;
-        }
-        int ans=prev[target];
-        return ans;
-    }
 };
