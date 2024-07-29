661eddcaac8b7cc3e67e611eb47e0a5c2470bf9c
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-29 20:01:58 +0530

    Time: 44 ms (47.30%), Space: 12.1 MB (84.14%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -41,25 +41,23 @@ public:
     int n = S1.size();
     int m = S2.size();
 
-    vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0]=true;
-        for(int i=1;i<=m;i++){
-            dp[0][i]=false;
-        }
-           for (int i = 1; i <= n; i++) {
-        dp[i][0] = isAllStars(S1, i);
-    }
+    vector<bool>prev(m+1,false),cur(m+1,false);
+        prev[0]=true;
+
         for(int i=1;i<=n;i++){
+                    cur[0] = isAllStars(S1, i);
             for(int j=1;j<=m;j++){
         if (S1[i-1] == S2[j-1] || S1[i-1] == '?')
-        dp[i][j] = dp[i - 1][j - 1];
+        cur[j] = prev[j - 1];
         else if (S1[i-1] == '*')
-        dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+        cur[j] = prev[j] || cur[j - 1];
         else
-        dp[i][j]=false;
+        cur[j]=false;
             
-        }}
-            return dp[n][m];
+        }
+        prev=cur;
+        }
+            return prev[m];
         
     }
 };
