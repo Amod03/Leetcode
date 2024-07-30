2a028b9d07dd6719b3790c159a7986beb74cb0f5
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-30 18:26:51 +0530

    Time: 10 ms (83.41%), Space: 13.1 MB (53.30%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
-        int n=a.size();
-     vector<int>dp(n,1),hash(n,0);
-       
-       for(int i=0;i<n;i++){
-           hash[i]=i;
-           for(int j=0;j<i;j++){
-               if(a[j]<a[i] && 1+dp[j]>dp[i]){
-                   dp[i]=1+dp[j];
-                   hash[i]=j;
-               }
-           }
-       }
-       int maxi=INT_MIN;
-       int last_index=-1;
-       for(int i=0;i<n;i++){
-           if(dp[i]>maxi){
-               maxi=dp[i];
-               last_index=i;
-           }
-       }
-        // printing
-       // vector<int>order;
-       // order.push_back(a[last_index]);
-       // while(hash[last_index]!=last_index){
-       //     last_index=hash[last_index];
-       //     order.push_back(a[last_index]);
-       // }
-       // reverse(order.begin(),order.end());
-    //   for(int i=0;i<order.size();i++){
-    //       cout<<order[i]<<" ";
-    //   }
-       return maxi;
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> temp;
+        temp.push_back(arr[0]);
+        int len = 1;
+        
+        for(int i=1; i<n; i++){
+            
+            if(arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+                len++;
+            }
+            else{
+                
+                int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+                temp[ind] = arr[i];
+                
+            }
+            
+        }
+        
+        return len;
     }
 };
