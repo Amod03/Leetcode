e9d8b0c6158bc052cbcb1218d9cb36ec6b449c9b
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-07-30 16:53:56 +0530

    Time: 85 ms (56.26%), Space: 13.4 MB (43.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-     vector<int> dp(n,1);
-    for(int i=0; i<=n-1; i++){
-        for(int prev_index = 0; prev_index <i; prev_index ++){
-            
-            if(nums[prev_index]<nums[i]){
-                dp[i] = max(dp[i], 1 + dp[prev_index]);
-            }
-        }
-    }
-    int ans = -1;
-    for(int i=0; i<=n-1; i++){
-        ans = max(ans, dp[i]);
-    }
-    return ans;
+    int lengthOfLIS(vector<int>& a) {
+        int n=a.size();
+     vector<int>dp(n,1),hash(n,0);
+       
+       for(int i=0;i<n;i++){
+           hash[i]=i;
+           for(int j=0;j<i;j++){
+               if(a[j]<a[i] && 1+dp[j]>dp[i]){
+                   dp[i]=1+dp[j];
+                   hash[i]=j;
+               }
+           }
+       }
+       int maxi=INT_MIN;
+       int last_index=-1;
+       for(int i=0;i<n;i++){
+           if(dp[i]>maxi){
+               maxi=dp[i];
+               last_index=i;
+           }
+       }
+        // printing
+       // vector<int>order;
+       // order.push_back(a[last_index]);
+       // while(hash[last_index]!=last_index){
+       //     last_index=hash[last_index];
+       //     order.push_back(a[last_index]);
+       // }
+       // reverse(order.begin(),order.end());
+    //   for(int i=0;i<order.size();i++){
+    //       cout<<order[i]<<" ";
+    //   }
+       return maxi;
     }
 };
