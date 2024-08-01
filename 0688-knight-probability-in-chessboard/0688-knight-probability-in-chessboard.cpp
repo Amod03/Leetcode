b4f8cf0d03a8b359881730db1acf0df37c2ed757
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-01 16:49:35 +0530

    Time: 17 ms (24.79%), Space: 10.9 MB (30.66%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -6,7 +6,7 @@ public:
          }
         if(move==0)
             return 1;
-        if(dp[iR][jC][move]!=-1 )return dp[iR][jC][move];
+        if(dp[move][iR][jC]!=-1 )return dp[move][iR][jC];
         double ways=0.0;
         for(int i=0;i<8;i++){
                 int newR=iR+a[i];
@@ -14,41 +14,35 @@ public:
                 ways+=(solve(newR,newC,n,move-1,a,b,dp))/8;
         }
         
-        return dp[iR][jC][move] = ways;
+        return dp[move][iR][jC] = ways;
     }
     
     double knightProbability(int n, int k, int row, int column) {
         int a[]={-2,-1,1,2,2,1,-1,-2};
         int b[]={1,2,2,1,-1,-2,-2,-1};
-        vector<vector<vector<double>>> dp(n, vector<vector<double>>(n, vector<double>(k+1, -1)));
+        vector<vector<vector<double>>> dp(k+1, vector<vector<double>>(n, vector<double>(n, 0)));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[0][i][j]=1;
+            }
+        }
+        
+        for (int move = 1; move <= k; ++move) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    for (int k = 0; k < 8; ++k) {
+                        int iR = i + a[k];
+                        int jC = j + b[k];
+                        if(iR < 0 || iR >=n || jC<0 || jC>=n){
+                              continue;
+                          }else{
+                        dp[move][i][j] = (dp[move][i][j] + dp[move-1][iR][jC]/8);         
+                        }
+                    }
+                }
+            }
+        }
 
-        return solve(row,column,n,k,a,b,dp);
+        return dp[k][row][column];
     }
 };
-// double count(int n, int k, int i, int j, vector<vector<vector<double>>>& dp)
-// {
-//     if(i >= n || j >= n || i < 0 || j < 0) {return 0;}
-//     if(k == 0) {return 1;}
-
-//     if(dp[k][i][j] != -1) {return dp[k][i][j];}
-    
-//     double dx[] = {1,1,2,2,-1,-1,-2,-2};
-//     double dy[] = {2,-2,1,-1,2,-2,1,-1};
-    
-//     double safe = 0;
-//     for(int l = 0; l < 8; l++)
-//     {
-//         int x = i + dx[l];
-//         int y = j + dy[l];
-//         safe += (count(n,k-1,x,y,dp)/8); 
-//     }
-
-//     return dp[k][i][j] = safe;
-// }
-
-// double knightProbability(int n, int k, int row, int column) 
-// {
-//    vector<vector<vector<double>>> dp(k+1, vector<vector<double>>(n, vector<double>(n, -1)));
-//     double safeProb = count(n,k,row,column, dp);
-//     return safeProb;
-// }
