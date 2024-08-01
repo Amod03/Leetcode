5806dff2ac589a4a57b7d1d29584752e72181c57
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-01 14:58:06 +0530

    Time: 7 ms (47.47%), Space: 11.3 MB (22.59%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int solve(int iR,int jC,int m,int n,int move,int a[],int b[],vector<vector<vector<int>>>&dp){
+        if(iR < 0 || iR >=m || jC<0 || jC>=n){
+                  return 1;
+         }
+        if(move==0)
+            return 0;
+        if(dp[iR][jC][move]!=-1 )return dp[iR][jC][move];
+        int ways=0;
+        for(int i=0;i<4;i++){
+                int newR=iR-a[i];
+                int newC=jC-b[i];
+                ways=(ways + solve(newR,newC,m,n,move-1,a,b,dp))%(1000000007);
+        }
+        
+        return dp[iR][jC][move]=ways;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int a[]={-1,0,1,0};
+        int b[]={0,1,0,-1};
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
+        return solve(startRow,startColumn,m,n,maxMove,a,b,dp);
+    }
+};
