b651ee34ed36d788a0db79bfed6a78db32ecc7f9
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-01 12:18:33 +0530

    Time: 87 ms (19.15%), Space: 20 MB (16.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -18,7 +18,16 @@ public:
     
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(0,nums,dp);
+        vector<int>dp(n,0);
+        
+        for(int i=n-2;i>=0;i--){
+            int mini=1e9;
+            for(int j=1;j<=nums[i];j++){
+                if(i+j<nums.size())
+                mini=min(mini,1+dp[i+j]);      
+           }
+          dp[i]=mini;
+        }
+        return dp[0];
     }
 };
