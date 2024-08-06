ad93240e0596e16c0f2e84d21cbb8cc42466fa7a
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-06 13:00:07 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (29.49%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -14,30 +14,28 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
       int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1)); // DP table to memoize results
+        vector<int>prev(m,-1),cur(m,-1); 
         for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            // Base conditions
             if (i >=0 && j >= 0 && obstacleGrid[i][j] == 1) {
-                dp[i][j] = 0; 
+                cur[j] = 0; 
                 continue;
             }
             if (i == 0 && j == 0) {
-                dp[i][j] = 1; 
+                cur[j] = 1; 
                 continue;
             }
             int up = 0;
             int left = 0;
-            // Check if we can move up and left (if not at the edge of the maze)
             if (i > 0)
-                up = dp[i - 1][j]; // Number of paths from above
+                up = prev[j]; 
             if (j > 0)
-                left = dp[i][j - 1]; // Number of paths from the left
-            // Total number of paths to reach (i, j) is the sum of paths from above and left
-            dp[i][j] = up + left;
+                left = cur[j - 1]; 
+            cur[j] = up + left;
         }
+            prev=cur;
     }
     // The final result is stored in dp[n-1][m-1], which represents the destination
-    return dp[n - 1][m - 1];
+    return prev[m - 1];
     }
 };
