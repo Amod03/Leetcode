575ff7755441824d14f4364815692c8d72304044
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-06 08:08:42 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (6.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,7 +17,7 @@ public:
         vector<int>dp(n+1,-1);
         dp[0]=1;
         dp[1]=1;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             int one=dp[i-1];
             int two=0;
             if(i-2>=0)
