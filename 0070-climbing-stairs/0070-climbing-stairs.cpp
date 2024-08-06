1336e3fa1bad3d6be15f0755243cce76b0712abf
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-06 08:08:26 +0530

    Time: 3 ms (29.50%), Space: 7.7 MB (6.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,6 +15,16 @@ public:
     
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return f(n,dp);
-    }
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=1;i<=n;i++){
+            int one=dp[i-1];
+            int two=0;
+            if(i-2>=0)
+                two=dp[i-2];
+            dp[i]=one+two;
+        }
+        return dp[n];
+
+        }
 };
