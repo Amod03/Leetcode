b1b5df81535386c5752ca0642908a5250b6501b7
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-08 17:10:58 +0530

    Time: 7 ms (71.22%), Space: 14 MB (48.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -37,7 +37,38 @@ int countPartitions(int n, int d, vector<int>& arr){
     return findWays(arr,(totSum-d)/2);
 }
     
-    int findTargetSumWays(vector<int>& nums, int target) {
-       return countPartitions(nums.size(),target,nums);
+    int findTargetSumWays(vector<int>& num, int d) {
+    int totSum = 0;
+                int n = num.size();
+
+    for(int i=0; i<n;i++){
+        totSum += num[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;  
+        int tar=(totSum-d)/2;
+        
+
+    vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken);
+        }
+    }
+    return dp[n-1][tar];
     }
 };
