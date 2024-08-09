1516ac7f090c2013c2751e1b3f36a4ce3aeb7e48
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-09 16:00:15 +0530

    Time: 68 ms (53.46%), Space: 73.9 MB (75.22%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int longestPalindromeSubseq(string text1) {
         string text2=text1;
-            reverse(text2.begin(),text2.end());
-        int n=text1.size();
-        int m=text2.size();
+        reverse(text2.begin(),text2.end());
+        int m=text1.size();
         vector<int>prev(m+1,0);
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=m;i++){
             vector<int>cur(m+1,0);
             for(int j=1;j<=m;j++){
             if(text1[i-1]==text2[j-1]){
