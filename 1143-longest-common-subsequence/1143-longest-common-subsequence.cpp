46728372471ff2a17eb2e4caf7687c960d05086b
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-09 12:53:26 +0530

    Time: 11 ms (98.86%), Space: 8 MB (92.97%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -16,18 +16,19 @@ public:
         int n = s1.size();
     int m = s2.size();
 
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); // Create a DP table
+    vector<int>prev(m + 1, 0); 
 
-    // Fill in the DP table to calculate the length of LCS
     for (int ind1 = 1; ind1 <= n; ind1++) {
+        int dia=0;
         for (int ind2 = 1; ind2 <= m; ind2++) {
+            int temp=prev[ind2];
             if (s1[ind1 - 1] == s2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
+                prev[ind2] = 1 + dia; 
             else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+                prev[ind2] = max(prev[ind2], prev[ind2 - 1]); 
+            dia=temp;
         }
     }
-
-    return dp[n][m];
+    return prev[m];
     }
 };
