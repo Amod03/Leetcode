ab643ce3d1411c70105a7c2176e373a3e63b2704
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2024-08-10 10:28:27 +0530

    Time: 2187 ms (5.01%), Space: 419.5 MB (14.35%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    
+    int countUtil(string s1, string s2, int ind1, int ind2, vector<vector<int>>& dp) {
+    if (ind2 < 0)
+        return 1;
+    if (ind1 < 0)
+        return 0;
+    if (dp[ind1][ind2] != -1)
+        return dp[ind1][ind2];
+
+    int result = 0;
+    if (s1[ind1] == s2[ind2]) {
+        int leaveOne = countUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
+        int stay = countUtil(s1, s2, ind1 - 1, ind2, dp);
+
+        result = (leaveOne + stay) ;
+    } else {
+        result = countUtil(s1, s2, ind1 - 1, ind2, dp);
+    }
+    dp[ind1][ind2] = result;
+    return result;
+}
+    
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<double>dp(m+1,0);
-        
-        dp[0]=1;
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=m;j>=1;j--)
-            {
-                if(s[i-1]==t[j-1]){
-                    dp[j]=dp[j-1]+dp[j];
-                }
-            }
-        }
-        return (int)dp[m];
+    vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
+    return countUtil(s, t, s.size() - 1, t.size() - 1, dp);
     }
 };
