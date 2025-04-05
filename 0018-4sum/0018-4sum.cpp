2a155cfbb49277cf53e4170b8f33eab7539462d8
Amod Katiyar <121477867+Amod03@users.noreply.github.com>
2025-04-05 20:33:40 +0530

    Time: 19 ms (78.81%), Space: 17.6 MB (32.65%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,19 +3,11 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size(); //size of the array
     vector<vector<int>> ans;
-
-    // sort the given array:
     sort(nums.begin(), nums.end());
-
-    //calculating the quadruplets:
     for (int i = 0; i < n; i++) {
-        // avoid the duplicates while moving i:
         if (i > 0 && nums[i] == nums[i - 1]) continue;
         for (int j = i + 1; j < n; j++) {
-            // avoid the duplicates while moving j:
             if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-            // 2 pointers:
             int k = j + 1;
             int l = n - 1;
             while (k < l) {
@@ -27,8 +19,6 @@ public:
                     vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
                     ans.push_back(temp);
                     k++; l--;
-
-                    //skip the duplicates:
                     while (k < l && nums[k] == nums[k - 1]) k++;
                     while (k < l && nums[l] == nums[l + 1]) l--;
                 }
@@ -37,7 +27,6 @@ public:
             }
         }
     }
-
     return ans;
     }
 };
